72ee1acc33677d09275d56c652d1b494e32d5052
OLeonardoRodrigues <oliveira.leonardo.r@gmail.com>
2022-07-05 00:44:07 -0300

    Fix env

--- src/auth.py
+++ src/auth.py
@@ -3,6 +3,6 @@ from os import system, environ
 def auth():
     try:
         print('Authenticating to GitHub.')
-        system(f"echo {environ.get('GH_PAT')} | gh auth login --git-protocol https --hostname GitHub.com --with-token")
+        system(f"echo {environ.get('GH_PAT')} | gh auth login --git-protocol https --hostname GitHub.com --with-token || exit 1")
     except:
         print("Failure authenticating to GitHub.")
--- src/set.py
+++ src/set.py
@@ -3,7 +3,7 @@ from os import system
 def set_topics(topics):
     try:
         print('Setting Repository Topics.')
-        system(f'gh repo edit {topics}')
+        system(f'gh repo edit {topics} || exit 1')
     except:
         print("Failure setting repository topics.")
 
@@ -11,6 +11,6 @@ def set_options(options):
     try:
         for option, value in options.items():
             print(f'Setting Repository Option: {option} with value: {value}.') if value else print(f'Setting Repository Option: {option}.')
-            system(f'gh repo edit --{option} {value}')
+            system(f'gh repo edit --{option} {value} || exit 1')
     except:
         print("Failure setting repository options.")
