545c0e2987d52257739fcbb693830c2b7fba582e
OLeonardoRodrigues <oliveira.leonardo.r@gmail.com>
2022-07-05 00:29:47 -0300

    Add Auth

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,11 @@
 from .parse_config import parse_config
 from .set import set_topics, set_options
+from .auth import auth
 
 def main():
     topics, options = parse_config()
+
+    auth()
 
     if(topics): set_topics(topics)
     if(options): set_options(options)
---
+++ src/auth.py
@@ -0,0 +1,8 @@
+from os import system, environ
+
+def auth():
+    try:
+        print('Authenticating to GitHub.')
+        system(f"echo {environ.get('GH_PAT')} | gh auth login --git-protocol https --hostname GitHub.com --with-token")
+    except:
+        print("Failure authenticating to GitHub.")
